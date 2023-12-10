c5983bf06e534067a375a0b7b6b7bd704fa3ebbf
Ajay Rawat <9ajayrawat@gmail.com>
2023-12-10 13:58:11 +0530

    A basic Todo React app is created.

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+export const App=()=>{
+  const [todos, modifyList]=useState([{id: 1, task: "Testing"}]);
+  const [todo, setTodo]=useState({id:null, task: null});
+
+  const handleTodoId=({target})=>{
+    setTodo({id:target.value, task:todo.task});
+  }
+
+  const handleTodoTask=({target})=>{
+    setTodo({id:todo.id, task:target.value});
+  }
+  
+  const handleKey=({key})=>{
+    if(key==="Enter") handleListAddToDo();
+  }
+
+  const handleListAddToDo=()=>{
+    console.log(todo);
+    modifyList(todos=>[...todos, todo]);
+    setTodo({id:0, task:""});
+    document.getElementById("newTodoId").value=null;
+    document.getElementById("newTodoTask").value=null;
+  }
+
+  const handleListRemoveToDo=(id)=>{
+    modifyList(todos.filter(task=>task.id!==id));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <ul>
+        { todos.map((todo)=>(
+            <li key={todo.id}>
+              <span>{todo.id} - {todo.task}</span>
+              <button onClick={()=>handleListRemoveToDo(todo.id)}>Remove</button>
+            </li>
+          )) }
+      </ul>
+      <title>Todo Id: </title>
+      <input type="number" name="newTodoId" id="newTodoId" placeholder='Todo Id' onChange={handleTodoId} />
+      <title>Todo Task: </title>
+      <input type="text" id="newTodoTask" name="newTodoTask" placeholder='Todo Task' onChange={handleTodoTask} onKeyDown={handleKey} />
+      <button type="submit" onClick={handleListAddToDo}>Add</button>
     </div>
   );
 }
